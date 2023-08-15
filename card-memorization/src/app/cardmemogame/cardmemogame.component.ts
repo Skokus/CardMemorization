a73e8cb3d5bca329b25675ef75f560a40556c18a
Paweł Cegielski <skok2022@gmail.com>
2023-08-15 20:42:42 +0200

    Checking solution

--- card-memorization/src/app/cardmemogame/cardmemogame.component.ts
+++ card-memorization/src/app/cardmemogame/cardmemogame.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CardmemogameComponent {
   cards = [];
+  actualOrder=["ace_of_spades", "2_of_diamonds", "3_of_clubs"];
+
+  onCheckButtonClick(): void{
+    console.log(this.checkTheSolution());
+  }
+
+  checkTheSolution(): boolean{
+    for(let i = 0; i < this.actualOrder.length; i++){
+      if(this.cards[i] != this.actualOrder[i]){
+        return false;
+      }
+    }
+    return true;
+  }
 }
