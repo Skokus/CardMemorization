3fa58e9a0279af71e4e207b2512c39a7bb820abe
Paweł Cegielski <skok2022@gmail.com>
2023-08-16 21:08:17 +0200

    Adding start and showing order

--- card-memorization/src/app/cardmemogame/cardmemogame.component.ts
+++ card-memorization/src/app/cardmemogame/cardmemogame.component.ts
@@ -8,11 +8,16 @@ import { Component } from '@angular/core';
 export class CardmemogameComponent {
   cards = [];
   actualOrder=["ace_of_spades", "2_of_diamonds", "3_of_clubs"];
+  isOrderShown = true;
 
   onCheckButtonClick(): void{
     console.log(this.checkTheSolution());
   }
 
+  onStartButtonClick(): void{
+    this.isOrderShown = !this.isOrderShown;
+  }
+  
   checkTheSolution(): boolean{
     for(let i = 0; i < this.actualOrder.length; i++){
       if(this.cards[i] != this.actualOrder[i]){
