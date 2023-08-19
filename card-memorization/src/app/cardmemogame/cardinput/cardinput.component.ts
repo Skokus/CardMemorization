f1cb12000b99667097633c9bd61d8fe4d9231ff3
Paweł Cegielski <skok2022@gmail.com>
2023-08-19 16:34:00 +0200

    Styling of card button

--- card-memorization/src/app/cardmemogame/cardinput/cardinput.component.ts
+++ card-memorization/src/app/cardmemogame/cardinput/cardinput.component.ts
@@ -14,7 +14,21 @@ export class CardinputComponent {
     { name: 'spades', symbol: '\u2660' }
   ];
   values : string[] = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
-
+  cardNames = [
+    { name: "ace", shortname: "A" },
+    { name: "2", shortname: "2" },
+    { name: "3", shortname: "3" },
+    { name: "4", shortname: "4" },
+    { name: "5", shortname: "5" },
+    { name: "6", shortname: "6" },
+    { name: "7", shortname: "7" },
+    { name: "8", shortname: "8" },
+    { name: "9", shortname: "9" },
+    { name: "10", shortname: "10" },
+    { name: "jack", shortname: "J" },
+    { name: "queen", shortname: "Q" },
+    { name: "king", shortname: "K" }
+  ];
   onClick(suit: string, value: string): void{
     this.cards.push(this.createCardName(suit, value));
   }
